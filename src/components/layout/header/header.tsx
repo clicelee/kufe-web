ba07fbfebb8ddc59dbed8557e5b554b8c281026d
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-12-07 21:35:26 +0900

    헤더 배경 색상 조정

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,18 +1,20 @@
 import Link from 'next/link';
 
+import { cn } from '@/lib/cn';
+
 const Header = () => {
   return (
-    <nav className="bg-transparent p-4 text-white">
-      <div className="container mx-auto flex items-center justify-between">
-        <div className="text-2xl font-bold">KUFE</div>
-        <ul className="flex space-x-6">
+    <nav className={cn('bg-[#036b3f] p-4 text-white')}>
+      <div className={cn('container mx-auto flex items-center justify-between')}>
+        <div className={cn('text-2xl font-bold')}>KUFE</div>
+        <ul className={cn('flex space-x-6')}>
           <li>
             <Link href="/articles">자료 공유</Link>
           </li>
           <li>
             <Link href="/jobs">채용 공고</Link>
           </li>
-          <li className="rounded bg-[#b0cda6] px-3 py-1 text-black">가입하기</li>
+          <li className={cn('rounded bg-[#b0cda6] px-3 py-1 text-black')}>가입하기</li>
         </ul>
       </div>
     </nav>
