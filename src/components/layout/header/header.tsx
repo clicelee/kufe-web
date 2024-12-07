c215117539dcf1f692108ae91ce63655493cb3e2
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2024-12-07 22:16:02 +0900

    페이지네이션 적용

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -7,12 +7,14 @@ const Header = () => {
   return (
     <nav className={cn('bg-[#036b3f] px-6 py-4 text-white')}>
       <div className={cn('container mx-auto flex items-center justify-between')}>
-        <Image src="/logo.png" alt="KUFE" width={80} height={80} />
+        <Link href="/">
+          <Image src="/logo.png" alt="KUFE" width={50} height={50} />
+        </Link>
         <ul className={cn('flex items-center space-x-6')}>
-          <li>
+          <li className={cn('font-bold hover:text-gray-300')}>
             <Link href="/articles">아티클 읽기</Link>
           </li>
-          <li>
+          <li className={cn('font-bold hover:text-gray-300')}>
             <Link href="/jobs">채용 공고</Link>
           </li>
         </ul>
